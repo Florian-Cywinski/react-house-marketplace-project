b75f966496ef43a694c956e75d5607d3cc075f39
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-17 20:56:26 +0200

    13 - House Marketplace Project Start -> 2 - React Setup & Firebase Config

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello from App.jsx to the House Marketplace App</h1>
+    </>
+  )
+}
+
+export default App
