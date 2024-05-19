7a0d0d23b76920534568b120a43d760e64bc612c
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-19 14:17:02 +0200

    14 - Firebase Authentication & Profile -> 1 - Sign In & Sign Up Form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,69 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+// The old way to import SVG's with CRA (create react app)
+// import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+// import visibilityIcon from '../assets/svg/visibilityIcon.svg'    // The other way to bring it in is because of it's just a source for an img tag
+// The new way to import SVG's with npm i vite-plugin-svgr and adapting the vite.config.js file
+import ArrowRightIcon from '../assets/svg/keyboardArrowRightIcon.svg?react'
+// import visibilityIcon from '../assets/svg/visibilityIcon.svg?react'    // This is not gonna work because it's used in an <img tag as src
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'   // This is the way to import the SVG for the usage as src in an img tag
+
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false) // [showPassword, setShowPassword] = [state, functionToSetTheState] - if it's true than the PW will be shown as text otherwise as ****
+  const [formData, setFormData] = useState({    // In this case the state is an object
+    email: '',
+    password: '',
+  })
+  const { email, password } = formData  // Destructuring of the formData that it's possible to use email directly instaed of formData.email
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {         // To update the form data state
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,  // To use the id it's needed to use [] - e.target.id can be email: od password:  
+    }))
+  }
+
+  const onSubmit = () => {
+
+  }
+
   return (
-    <div>Sign In</div>
+    <>  {/* This fragment is used to be able to bring in more that one tag / component */}
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+
+        <form onSubmit={onSubmit}>
+          <input type='email' className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange}/> {/* The id is to be able to use it in the onChange function -> e.target.id */}
+
+          <div className='passwordInputDiv'>
+            <input type={showPassword ? 'text' : 'password'} className='passwordInput' placeholder='Password' id='password' value={password} onChange={onChange}/>  {/* if it's true than the PW will be shown as text otherwise as **** */}
+
+            <img src={visibilityIcon}   // To bring in the SVG
+              alt='show password' className='showPassword'
+              onClick={() => setShowPassword((prevState) => !prevState)}  // To set it to true if it's false and vice versa
+            />
+          </div>
+
+          <Link to='/forgot-password' className='forgotPasswordLink'>Forgot Password</Link>
+
+          <div className='signInBar'>
+            <p className='signInText'>Sign In</p>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+
+        {/* <OAuth />   Google OAuth */}
+
+        <Link to='/sign-up' className='registerLink'>Sign Up Instead</Link>
+      </div>
+    </>
   )
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,71 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+// The old way to import SVG's with CRA (create react app)
+// import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+// import visibilityIcon from '../assets/svg/visibilityIcon.svg'    // The other way to bring it in is because of it's just a source for an img tag
+// The new way to import SVG's with npm i vite-plugin-svgr and adapting the vite.config.js file
+import ArrowRightIcon from '../assets/svg/keyboardArrowRightIcon.svg?react'
+// import visibilityIcon from '../assets/svg/visibilityIcon.svg?react'    // This is not gonna work because it's used in an <img tag as src
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'   // This is the way to import the SVG for the usage as src in an img tag
+
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false) // [showPassword, setShowPassword] = [state, functionToSetTheState] - if it's true than the PW will be shown as text otherwise as ****
+  const [formData, setFormData] = useState({    // In this case the state is an object
+    name: '',
+    email: '',
+    password: '',
+  })
+  const { name, email, password } = formData  // Destructuring of the formData that it's possible to use email directly instaed of formData.email
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  const onSubmit = () => {
+
+  }
+
   return (
-    <div>Sign Up</div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+
+        <form onSubmit={onSubmit}>
+          <input type='text' className='nameInput' placeholder='Name' id='name' value={name} onChange={onChange}/>  {/* The id is to be able to use it in the onChange function -> e.target.id */}
+          <input type='email' className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange}/>
+
+          <div className='passwordInputDiv'>
+            <input type={showPassword ? 'text' : 'password'} className='passwordInput' placeholder='Password' id='password' value={password} onChange={onChange}/>  {/* if it's true than the PW will be shown as text otherwise as **** */}
+
+            <img src={visibilityIcon} // To bring in the SVG
+              alt='show password' className='showPassword'
+              onClick={() => setShowPassword((prevState) => !prevState)}  // To set it to true if it's false and vice versa
+            />
+          </div>
+
+          <Link to='/forgot-password' className='forgotPasswordLink'>Forgot Password</Link>
+
+          <div className='signUpBar'>
+            <p className='signUpText'>Sign Up</p>
+            <button className='signUpButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+
+        {/* Google OAuth */}
+
+        <Link to='/sign-in' className='registerLink'>Sign In Instead</Link>
+      </div>
+    </>
   )
 }
 
