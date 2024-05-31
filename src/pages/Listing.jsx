c81d27724202af51d99b51b4ed6b0463dac7fd38
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-31 15:56:11 +0200

    16 - Single Listing, Map, Slider & Edit -> 2 - Listing Details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,7 +11,7 @@ function Listing() {
   const [loading, setLoading] = useState(true)
   const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const params = useParams()
   const auth = getAuth()
 
@@ -20,10 +20,8 @@ function Listing() {
       const docRef = doc(db, 'listings', params.listingId)  // listings is the collection from firestore - params.listingId is the listings id from the url
       const docSnap = await getDoc(docRef)  // To have a snapshot of docRef
 
-      console.log(docSnap.exists());
-
       if (docSnap.exists()) { // To check whether this documents exists
-        console.log(docSnap.data());
+        // console.log(docSnap.data());
         setListing(docSnap.data())
         setLoading(false)
       }
@@ -32,8 +30,67 @@ function Listing() {
     fetchListing()
   }, [params.listingId])
 
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
-    <div>Listing</div>
+    <main>
+      {/* To be able to copy the link to the clipboard */}
+      <div className='shareIconDiv'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href) // window.location.href is the url - navigator.clipboard.writeText to bring in the link
+          // To have a message that the link has been copied (it's true for 2 sec.)
+          setShareLinkCopied(true)
+          setTimeout(() => {  
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+        ><img src={shareIcon} alt='' />
+      </div>
+      {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}   {/* To show the message that the link was copied for 2 sec. - http://127.0.0.1:3000/category/sale/6F0JPSosb2vsRMrTKEGw */}
+
+      {/* To display the Listing Details */}
+      <div className='listingDetails'>
+        <p className='listingName'>
+          {listing.name} - $
+          {listing.offer  // To show either the discounted or the regular price
+            ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')  // To have a Thousands separator
+            : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.location}</p>
+        <p className='listingType'>For {listing.type === 'rent' ? 'Rent' : 'Sale'}</p>
+        {/* To show the discount between the regular and the offer price */}
+        {listing.offer && (<p className='discountPrice'>${listing.regularPrice - listing.discountedPrice} discount</p>
+        )}
+        {/* To list the number of bedrooms etc */}
+        <ul className='listingDetailsList'>
+          <li>{listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}</li>
+          <li>{listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}</li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+
+        {/* To show the location */}
+        <p className='listingLocationTitle'>Location</p>    {/* The heading for the map */}
+        {/* To do - Map */}
+
+        {/* To have a Contact button (just if the listing isn't a listing from the logged in user) */}
+        {auth.currentUser?.uid !== listing.userRef && (     // if the listing isn't a listing from the logged in user - the ? to avoid getting an error saying it's null
+          <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className='primaryButton'>Contact Landlord</Link>  // ?listingName=${listing.name} is a query string - e.g. /contact/UqI39...k1?listingName=Flowery%20Branch%20Beauty
+        )}
+
+
+      </div>
+
+
+
+
+
+
+
+
+    </main>
  
   )
 }
